6370871cca3cabf2e8ecb4ce0d4ed52920b9cf53
Murali <muraliece_88@yahoo.com>
2024-08-20 11:11:19 +0200

    disabled ssl

--- ABNOrders/productOrders/src/main/java/com/abn/nl/exceptions/GlobalExceptionHandlers.java
+++ ABNOrders/productOrders/src/main/java/com/abn/nl/exceptions/GlobalExceptionHandlers.java
@@ -21,8 +21,8 @@ public class GlobalExceptionHandlers {
     @ExceptionHandler(ProductNotFoundException.class)
     public ResponseEntity<Errors> handleprodutException(ProductNotFoundException ex) {
         log.error("exception occured{}", ex.getMessage());
-        Errors errors=new Errors(HttpStatus.INTERNAL_SERVER_ERROR.value(), ex.getMessage(), LocalDateTime.now());
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(errors);
+        Errors errors=new Errors(HttpStatus.NOT_FOUND.value(), ex.getMessage(), LocalDateTime.now());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(errors);
     }
     @ExceptionHandler(ProxyNotFoundException.class)
     public ResponseEntity<Errors> handleProxyException(ProxyNotFoundException ex) {
