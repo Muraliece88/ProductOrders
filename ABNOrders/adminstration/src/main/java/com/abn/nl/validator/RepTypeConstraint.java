4c6a99609e518721d7b06ee86ba1b29842eed63a
Murali <muraliece_88@yahoo.com>
2024-08-20 07:48:23 +0200

    Modified to the docker files

--- ABNOrders/adminstration/src/main/java/com/abn/nl/validator/RepTypeConstraint.java
+++ ABNOrders/adminstration/src/main/java/com/abn/nl/validator/RepTypeConstraint.java
@@ -1,6 +1,7 @@
 package com.abn.nl.validator;
 
 import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -11,4 +12,9 @@ import java.lang.annotation.Target;
 @Target({ElementType.PARAMETER, ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface RepTypeConstraint {
+    String message() default "Invalid reporting type";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
 }
